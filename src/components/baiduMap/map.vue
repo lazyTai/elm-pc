c3707422b0be4fa7887bc0e695827e56743a4128
lazyTai <shuaigeliumingtai@163.com>
2017-06-29 14:40:25 +0800

    百度地图

---
+++ src/components/baiduMap/map.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="baiduMap"></div>
+</template>
+<script>
+  export  default{
+    mounted: function () {
+      var map = new BMap.Map("baiduMap");          // 创建地图实例
+      var point = new BMap.Point(116.404, 39.915);  // 创建点坐标
+      map.centerAndZoom(point, 15);
+      window.setTimeout(function () {
+        map.panTo(new BMap.Point(116.409, 39.918));
+      }, 2000);
+    }
+  }
+</script>
+<style scoped="true">
+  #baiduMap {
+    width: 300px;
+    height: 300px
+  }
+</style>
