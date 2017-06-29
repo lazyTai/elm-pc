c211c89fef3341c14a35545a33cee8e2ff0255fb
lazyTai <shuaigeliumingtai@163.com>
2017-06-29 14:52:27 +0800

    MapBaidu2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <MapBaidu></MapBaidu>
+    <MapBaidu2></MapBaidu2>
+  </div>
+</template>
+<style>
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+</style>
+<script>
+  import MapBaidu from './components/baiduMap/map.vue'
+  import MapBaidu2 from './components/baiduMap/2控件/map.vue'
+  export default {
+    name: 'app',
+    components: {MapBaidu, MapBaidu2}
+  }
+</script>
+
+
