c6dc2a82e519cf0984877feb9cba712bb211918f
lazyTai <shuaigeliumingtai@163.com>
2017-06-29 14:47:56 +0800

    MapBaidu2

--- src/components/baiduMap/2控件/map.vue
+++ src/components/baiduMap/2控件/map.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id=""></div>
+  <div id="demo">
+    <h1>
+      向地图添加控件
+    </h1>
+    <div id="cantainer-2"></div>
+  </div>
 </template>
-<script></script>
-<style></style>
+<script>
+  export default {
+    mounted: function () {
+      var map = new BMap.Map('cantainer-2');
+      map.centerAndZoom(new BMap.Point(116.404, 39.915), 11)
+      map.addControl(new BMap.NavigationControl())
+    }
+  }
+</script>
+<style scroped>
+  #cantainer-2 {
+    width: 300px;
+    height: 300px
+  }
+</style>
